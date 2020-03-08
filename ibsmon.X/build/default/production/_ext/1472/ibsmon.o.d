799f79aebb98bff8cabd8272d951a3dd497d407f
root <root@scsitest.microchip.com>
2020-03-08 07:51:34 -0700

    up

---
+++ ibsmon.X/build/default/production/_ext/1472/ibsmon.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/ibsmon.o:  \
+	/fred/ibs_monitor/pat/ibsmon.c  \
+	/fred/ibs_monitor/pat/blinker.h  \
+	/fred/ibs_monitor/pat/ibsmon.h  \
+
---
+++ ibsmon.X/build/default/production/_ext/1472/ihc_vector.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1472/ihc_vector.o:  \
+	/fred/ibs_monitor/pat/ihc_vector.c  \
+	/fred/ibs_monitor/pat/ihc_vector.h  \
+	/fred/ibs_monitor/pat/ibsmon.h  \
+
