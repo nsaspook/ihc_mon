b9ffb8ec1ff55f3ff8b964be077a593a36996b82
nsaspook <nsaspook@nsaspook.com>
2020-03-14 08:18:03 -0700

    up

--- ibsmon.X/build/default/production/_ext/1472/ibsmon.o.d
+++ ibsmon.X/build/default/production/_ext/1472/ibsmon.o.d
@@ -1,6 +1,6 @@
 build/default/production/_ext/1472/ibsmon.o:  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/ibsmon.c  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/ihc_vector.h  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/crc.h  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/ibsmon.h  \
+	/root/ihc_mon/ibsmon.c  \
+	/root/ihc_mon/ihc_vector.h  \
+	/root/ihc_mon/crc.h  \
+	/root/ihc_mon/ibsmon.h  \
 
