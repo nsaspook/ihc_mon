c9d027356c654285402086732f6936161b4def4c
nsaspook <nsaspook@nsaspook.com>
2020-03-21 13:05:57 -0700

    up

--- ibsmon.X/build/default/production/_ext/1472/crc.o.d
+++ ibsmon.X/build/default/production/_ext/1472/crc.o.d
@@ -1,5 +1,5 @@
 build/default/production/_ext/1472/crc.o:  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/crc.c  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/crc.h  \
-	/root/MPLABXProjects/vtouch_v2/ihc_mon/ibsmon.h  \
+	/root/ihc_mon/crc.c  \
+	/root/ihc_mon/crc.h  \
+	/root/ihc_mon/ibsmon.h  \
 
